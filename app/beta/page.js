a7e844137b96967f8e3b56251a241e160af879dc
idemirtas28 <idemirtas28@my.whitworth.edu>
2025-04-22 14:19:47 -0700

    hotfix

--- app/beta/page.js
+++ app/beta/page.js
@@ -1,44 +1,11 @@
-"use client";
-import { useEffect, useState } from "react";
-import Link from "next/link";
 import PdfDisplay from "@/components/PDFDisplay";
 
 export default function Beta() {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const response = await fetch("/syllabi-reader.users.json", {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const data = await response.json();
-        setUsers(data);
-        setLoading(false);
-      } catch (e) {
-        setError(e);
-        setLoading(false);
-      }
-    };
-    fetchUsers();
-  }, []);
-
   return (
     <main>
-      <header>
-        <h1 className="flex justify-self-center text-4xl font-bold my-10">
-          Syllabi-Reader
-        </h1>
-      </header>
-      <PdfDisplay />
+      <div>
+        <PdfDisplay />
+      </div>
     </main>
   );
 }
